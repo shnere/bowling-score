9c215a08accf1ab7af40b4a0cab507f36f171c1d
shnere <alanrodriguezromero@gmail.com>
2015-07-22 23:32:41 -0500

    Initial commit

---
+++ bowling.js
@@ -0,0 +1,13 @@
+var ScoreCard = function(rolls){
+    this.rolls = rolls;
+};
+
+
+Score.prototype.calculateResult = function(){
+    if(!Array.isArray(this.rolls)){
+        return false;
+    }
+    var scoreCard = [];
+};
+
+var score = new ScoreCard([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9]);
