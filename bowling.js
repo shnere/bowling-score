f195fb10ab7882e30ceccc9ac2a323897a4c7fa2
shnere <alanrodriguezromero@gmail.com>
2015-07-25 21:46:40 -0500

    Added translation to bidimensional array for easier graphic representation

--- bowling.js
+++ bowling.js
@@ -1,13 +1,40 @@
 var ScoreCard = function(rolls){
     this.rolls = rolls;
+
+    var that = this;
+    // Translate rolls into a bidimensional array
+    this.translateRolls = function(){
+        var scoreCard = [],
+            currentScore = 0,
+            val;
+
+        for(var i = 0; i < that.rolls.length; i++){
+            val = that.rolls[i];
+            if(currentScore > 8){
+                if(Array.isArray(scoreCard[currentScore])){
+                    scoreCard[currentScore].push(val);
+                }else{
+                    scoreCard[currentScore] = [val];
+                }
+            }else if(val < 10){
+                scoreCard[currentScore] = [val, that.rolls[i + 1]];
+                i++;
+                currentScore++;
+            }else if(val === 10){
+                scoreCard[currentScore] = [val];
+                currentScore++;
+            }
+        }
+        return scoreCard;
+    };
 };
 
 
-Score.prototype.calculateResult = function(){
+ScoreCard.prototype.calculateResult = function(){
     if(!Array.isArray(this.rolls)){
         return false;
     }
-    var scoreCard = [];
+
 };
 
 var score = new ScoreCard([9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9,1, 9]);
